15446d8c8405992f39760095f31c07b93508ac31
Marco <marco.salman@gmail.com>
2022-04-19 00:02:43 -0400

    SQL injection security

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,16 +7,20 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+
+const cohortName = process.argv[2];
+const values = [`${cohortName}`];
+
 pool.query(`
 SELECT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2]}'
+WHERE cohorts.name = $1
 GROUP BY teachers.name, cohorts.name
 ORDER BY teacher;
-`)
+`, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.teacher}`);
